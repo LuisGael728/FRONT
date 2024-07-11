07322aff197763a89eee316ec9b1fa274b2cfdd2
Sebastian Gomez Calvario <54549298+sebas0000@users.noreply.github.com>
2024-07-11 13:12:13 -0600

    Revert "Tablas"

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import './Header.css';
-import logo from '../../assets/menu_icono.svg';
 import logoCFE from '../../assets/CFE.svg';
-import menuIcon from '../../assets/menu_icono.svg';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,16 +11,6 @@ const Header = () => {
 
   return (
     <nav className="navbar">
-      <div className="logo-container">
-        <img src={logo} alt="Logo" className="logo" onClick={toggleMenu} />
-      </div>
-      <div className="title">
-        <a href="#">Sistema de seguimiento de asistencia en vivo.</a>
-      </div>
-      <div className="logo-container">
-        <img src={logoCFE} alt="logoCFE" className="logo" />
-      </div>
-      <img src={menuIcon} alt="Menu Icon" className="menu-icon" />
       <div className="nav-container">
         <div className="hamburger-lines" onClick={toggleMenu}>
           <span className={`line line1 ${isMenuOpen ? 'open' : ''}`}></span>
@@ -35,10 +23,10 @@ const Header = () => {
         <div className="logo-container">
           <img src={logoCFE} alt="logo CFE" className="logo" />
         </div>
-        <ul className={`menu-items ${isMenuOpen ? 'open' : ''}`}>
+        <div className={`menu-items ${isMenuOpen ? 'open' : ''}`}>
           <li><a href="/">Entrada</a></li>
           <li><a href="/">Salida</a></li>
-        </ul>
+        </div>
       </div>
     </nav>
   );
