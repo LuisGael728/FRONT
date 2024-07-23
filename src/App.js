fab8dd9fff81c0a756bab893df320b84c512b113
sebas0000 <sgomez4@ucol.mx>
2024-07-23 14:34:18 -0600

    creacion de alerta

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Swal from 'sweetalert2';
 import Layout from './components/Layout/Layout';
-
+import './index.css'
 function App() {
+  useEffect(() => {
+    Swal.fire({
+      title: 'Sin tablas',
+      width: 600,
+      text: 'Seleccione una tabla en el menu lateral',
+      icon:'warning',
+      confirmButtonText: 'Entiendo',
+    });
+  },); 
+
   return (
-    <Layout/>
+      <Layout />
   );
 }
 
